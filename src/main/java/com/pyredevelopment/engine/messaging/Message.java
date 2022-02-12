ecd37a2aeb631ebc02f5e685ccb3fb0709b304ba
Luke Hanna <luke.m.hanna@gmail.com>
2022-02-12 08:08:21 -0500

    Started setting up a class for all systems to extend

---
+++ src/main/java/com/pyredevelopment/engine/messaging/Message.java
@@ -0,0 +1,4 @@
+package com.pyredevelopment.engine.messaging;
+
+public class Message {
+}
---
+++ src/main/java/com/pyredevelopment/engine/systems/SuperSystem.java
@@ -0,0 +1,38 @@
+package com.pyredevelopment.engine.systems;
+
+import com.pyredevelopment.engine.messaging.Message;
+
+public class SuperSystem implements Runnable {
+
+    private volatile boolean systemRunning = false;
+
+    private SuperSystem() {
+        startup();
+        Thread systemThread = new Thread(this);
+        systemThread.start();
+    }
+
+    public void startup() {
+
+    }
+
+    public void systemLoop() {
+        
+    }
+
+    public void sendMessage(Message msg) {
+
+    }
+
+    public void handleMessage(Message msg) {
+
+    }
+
+    @Override
+    public void run() {
+        systemRunning = true;
+        while (systemRunning) {
+            System.out.println("System running!");
+        }
+    }
+}
