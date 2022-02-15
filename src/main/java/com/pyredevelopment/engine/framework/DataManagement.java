b1cc4ceb2de1a879c9ac054a0e6834bbdf5b580a
Luke Hanna <luke.m.hanna@gmail.com>
2022-02-14 19:18:19 -0500

    added some datamanagement methods

--- src/main/java/com/pyredevelopment/engine/framework/DataManagement.java
+++ src/main/java/com/pyredevelopment/engine/framework/DataManagement.java
@@ -1,4 +1,16 @@
 package com.pyredevelopment.engine.framework;
 
 public class DataManagement {
+
+    public static byte toByte(String string) {
+        return Byte.parseByte(string, 2);
+    }
+
+    public static byte toByte(boolean b1, boolean b2, boolean b3, boolean b4,
+                              boolean b5, boolean b6, boolean b7, boolean b8) {
+        return (byte)((b1?1<<7:0) + (b2?1<<6:0) + (b3?1<<5:0) +
+                (b4?1<<4:0) + (b5?1<<3:0) + (b6?1<<2:0) +
+                (b7?1<<1:0) + (b8?1:0));
+    }
+
 }
