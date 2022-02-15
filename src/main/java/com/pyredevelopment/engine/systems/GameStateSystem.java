fbae20be4cdfeb25f95d755865f0fd70081b66ba
Luke Hanna <luke.m.hanna@gmail.com>
2022-02-15 08:45:55 -0500

    Created main game system

---
+++ src/main/java/com/pyredevelopment/engine/systems/GameStateSystem.java
@@ -0,0 +1,24 @@
+package com.pyredevelopment.engine.systems;
+
+public class GameStateSystem extends SuperSystem {
+
+    @Override
+    public void startup() {
+
+    }
+
+    @Override
+    public void systemLoop() {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+
+    public void handleKeyUpdate(byte[] update) {
+
+    }
+}
